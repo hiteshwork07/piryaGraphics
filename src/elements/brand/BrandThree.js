3bb9d9731bc230d1a863372084371f09dc8730e2
Bhautik <bhautik.tech@gmail.com>
2023-06-19 15:38:34 +0530

    changes logo, add map

--- src/elements/brand/BrandThree.js
+++ src/elements/brand/BrandThree.js
@@ -1,34 +1,57 @@
 import React from 'react';
 
+// const BrandList = [
+//     {
+//         image: './images/brand/brand-01.png'
+//     },
+//     {
+//         image: './images/brand/brand-02.png'
+//     },
+//     {
+//         image: './images/brand/brand-03.png'
+//     },
+//     {
+//         image: './images/brand/brand-04.png'
+//     },
+//     {
+//         image: './images/brand/brand-05.png'
+//     },
+//     {
+//         image: './images/brand/brand-06.png'
+//     },
+//     {
+//         image: './images/brand/brand-07.png'
+//     },
+//     {
+//         image: './images/brand/brand-08.png'
+//     },
+//     {
+//         image: './images/brand/brand-01.png'
+//     },
+//
+// ]
+
 const BrandList = [
-    {
-        image: './images/brand/brand-01.png'
-    },
-    {
-        image: './images/brand/brand-02.png'
-    },
-    {
-        image: './images/brand/brand-03.png'
-    },
-    {
-        image: './images/brand/brand-04.png'
-    },
-    {
-        image: './images/brand/brand-05.png'
-    },
-    {
-        image: './images/brand/brand-06.png'
-    },
-    {
-        image: './images/brand/brand-07.png'
-    },
-    {
-        image: './images/brand/brand-08.png'
-    },
-    {
-        image: './images/brand/brand-01.png'
-    },
-  
+    {image: './images/client_logo/1.png'},
+    {image: './images/client_logo/2.png'},
+    {image: './images/client_logo/3.png'},
+    {image: './images/client_logo/4.png'},
+    {image: './images/client_logo/5.png'},
+    {image: './images/client_logo/6.png'},
+    {image: './images/client_logo/7.png'},
+    {image: './images/client_logo/8.png'},
+    {image: './images/client_logo/9.png'},
+    {image: './images/client_logo/10.png'},
+    {image: './images/client_logo/11.png'},
+    {image: './images/client_logo/12.png'},
+    {image: './images/client_logo/13.png'},
+    {image: './images/client_logo/14.png'},
+    {image: './images/client_logo/15.png'},
+    {image: './images/client_logo/16.png'},
+    {image: './images/client_logo/17.png'},
+    {image: './images/client_logo/18.png'},
+    {image: './images/client_logo/19.png'},
+    {image: './images/client_logo/20.png'}
 ]
 
 const BrandThree = ({brandStyle}) => {
--- src/elements/contact/GoogleMapStyle.js
+++ src/elements/contact/GoogleMapStyle.js
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import {withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 class GoogleMapStyle extends Component {
   static defaultProps = {
     center: {
-      lat: 21.225487,
-      lng: 72.896577
+      lat: 21.21883391157044,
+      lng: 72.89539965247432
     },
     zoom: 11
   };
 
   render() {
+
+    const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{ lat: 21.21883391157044, lng: 72.89539965247432 }}
+        >
+          <Marker
+              position={{ lat: 21.21883391157044, lng: 72.89539965247432 }}
+          />
+        </GoogleMap>
+    ));
+
     return (
       // Important! Always set the container height explicitly
       <div className="google-map-style-1">
-        <GoogleMapReact
-          // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
-          defaultCenter={this.props.center}
-          defaultZoom={20}
-        >
-          <AnyReactComponent
-            lat={21.225487}
-            lng={72.896577}
-            text="My Marker"
-          />
-        </GoogleMapReact>
+        {/*<GoogleMapReact*/}
+        {/*  bootstrapURLKeys={{ key: 'AIzaSyD-G_YCv_F45rntfXcLgOM3jsZcHRGKt8U' }}*/}
+        {/*  defaultCenter={this.props.center}*/}
+        {/*  defaultZoom={20}*/}
+        {/*>*/}
+        {/*  <AnyReactComponent*/}
+        {/*    lat={21.225487}*/}
+        {/*    lng={72.896577}*/}
+        {/*    text="My Marker"*/}
+        {/*  />*/}
+        {/*</GoogleMapReact>*/}
+
+        <MapWithAMarker
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-G_YCv_F45rntfXcLgOM3jsZcHRGKt8U&v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+        />
+
       </div>
     );
   }
